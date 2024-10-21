bbc0e5d203804e3b5dbf5516e7e8aa70a2cb7846
MiThetThetKhaing <mithetthetkhaing.lashio@gmail.com>
2024-10-21 17:03:11 +0630

    feat: Add TodoViewModel

---
+++ MTTKDotNetCore.TodoListRestAPI/ViewModels/ToDoViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace MTTKDotNetCore.TodoListRestAPI.ViewModels
+{
+    public class ToDoViewModel
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public int? CategoryID { get; set; }
+        public int? PriorityLevel { get; set; }
+        public string? Status { get; set; }
+        public DateTime? DueDate { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public DateTime? CompletedDate { get; set; }
+    }
+}
