dacb70cc9a0d005dbfcf78f8181dbf455ec3481a
MiThetThetKhaing <mithetthetkhaing.lashio@gmail.com>
2024-10-21 17:02:51 +0630

    feat: Add TodoDataModel

---
+++ MTTKDotNetCore.TodoListRestAPI/DataModels/ToDoDataModel.cs
@@ -0,0 +1,15 @@
+﻿namespace MTTKDotNetCore.TodoListRestAPI.DataModels
+{
+    public class ToDoDataModel
+    {
+        public int TaskID { get; set; }
+        public string TaskTitle { get; set; }
+        public string TaskDescription { get; set; }
+        public int CategoryID { get; set; }
+        public string PriorityLevel { get; set; }
+        public string Status { get; set; }
+        public DateTime DueDate { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime CompletedDate { get; set; }
+    }
+}
