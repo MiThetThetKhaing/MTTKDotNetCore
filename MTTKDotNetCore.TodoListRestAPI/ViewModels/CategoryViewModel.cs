a35369e95350a4ca36268aabd1a37628bbfef4cf
MiThetThetKhaing <mithetthetkhaing.lashio@gmail.com>
2024-10-21 11:18:57 +0630

    feat: Add CategoryViewModel

---
+++ MTTKDotNetCore.TodoListRestAPI/ViewModels/CategoryViewModel.cs
@@ -0,0 +1,13 @@
+﻿using MTTKDotNetCore.Database.Models;
+
+namespace MTTKDotNetCore.TodoListRestAPI.ViewModels
+{
+    public class CategoryViewModel
+    {
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+
+        //public virtual ICollection<ToDoList> ToDoLists { get; set; }
+    }
+}
