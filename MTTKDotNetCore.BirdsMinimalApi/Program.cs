d76cbc1321d14ca3c7a7ea1e581700eb0c93da04
MiThetThetKhaing <mithetthetkhaing.lashio@gmail.com>
2024-11-06 15:59:57 +0630

    feat: add birdsMinimalApi

---
+++ MTTKDotNetCore.BirdsMinimalApi/Program.cs
@@ -0,0 +1,106 @@
+using Newtonsoft.Json;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast")
+.WithOpenApi();
+
+app.MapGet("/birds", () =>
+{
+    string folderPath = "Data/Birds.json";
+    var jsonStr = File.ReadAllText(folderPath);
+    var result = JsonConvert.DeserializeObject<BirdResponseModel>(jsonStr)!;
+
+    return Results.Ok(result.Tbl_Bird);
+})
+.WithName("GetBirds")
+.WithOpenApi();
+
+app.MapGet("/birds/{id}", (int id) =>
+{
+    string folderPath = "Data/Birds.json";
+    var jsonStr = File.ReadAllText(folderPath);
+    var result = JsonConvert.DeserializeObject<BirdResponseModel>(jsonStr)!;
+
+    var item = result.Tbl_Bird.FirstOrDefault(x => x.Id == id);
+    if (item is null) return Results.BadRequest("No data found!");
+
+    return Results.Ok(item);
+})
+.WithName("GetBird")
+.WithOpenApi();
+
+//app.MapPost("/birds", (BirdModel bird) =>
+//{
+//    string folderPath = "Data/Birds.json";
+//    var model = new BirdModel()
+//    {
+//        Id = bird.Id,
+//        BirdMyanmarName = bird.BirdMyanmarName,
+//        BirdEnglishName = bird.BirdEnglishName,
+//        Description = bird.Description,
+//        ImagePath = bird.ImagePath
+//    };
+//    string result = JsonConvert.SerializeObject(model, Formatting.Indented);
+//    var item = JsonConvert.DeserializeObject<BirdModel>(result);
+    
+//    return Results.Ok(item);
+//})
+//.WithName("CreateBird")
+//.WithOpenApi();
+
+app.Run();
+
+internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
+
+
+public class BirdResponseModel
+{
+    public BirdModel[] Tbl_Bird { get; set; }
+}
+
+public class BirdModel
+{
+    public int Id { get; set; }
+    public string BirdMyanmarName { get; set; }
+    public string BirdEnglishName { get; set; }
+    public string Description { get; set; }
+    public string ImagePath { get; set; }
+}
+
