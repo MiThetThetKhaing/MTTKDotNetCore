264addd297894a99d6030ccc841d3b42a99059d3
MiThetThetKhaing <mithetthetkhaing.lashio@gmail.com>
2024-11-16 17:32:01 +0630

    feat: ReUpdate Database

--- MTTKDotNetCore.Database/Models/AppDbContext.cs
+++ MTTKDotNetCore.Database/Models/AppDbContext.cs
@@ -21,6 +21,18 @@ public partial class AppDbContext : DbContext
 
     public virtual DbSet<TblBlog> TblBlogs { get; set; }
 
+    public virtual DbSet<TblProduct> TblProducts { get; set; }
+
+    public virtual DbSet<TblProductCategory> TblProductCategories { get; set; }
+
+    public virtual DbSet<TblSaleInvoice> TblSaleInvoices { get; set; }
+
+    public virtual DbSet<TblSaleInvoiceDetail> TblSaleInvoiceDetails { get; set; }
+
+    public virtual DbSet<TblShop> TblShops { get; set; }
+
+    public virtual DbSet<TblStaff> TblStaffs { get; set; }
+
     public virtual DbSet<TblTransactionHistory> TblTransactionHistories { get; set; }
 
     public virtual DbSet<ToDoList> ToDoLists { get; set; }
@@ -63,6 +75,114 @@ public partial class AppDbContext : DbContext
 
             entity.Property(e => e.BlogAuthor).HasMaxLength(50);
             entity.Property(e => e.BlogTitle).HasMaxLength(50);
+        });
+
+        modelBuilder.Entity<TblProduct>(entity =>
+        {
+            entity.HasKey(e => e.ProductId);
+
+            entity.ToTable("Tbl_Product");
+
+            entity.Property(e => e.Price).HasColumnType("decimal(18, 2)");
+            entity.Property(e => e.ProductCategoryCode).HasMaxLength(50);
+            entity.Property(e => e.ProductCode).HasMaxLength(50);
+            entity.Property(e => e.ProductName).HasMaxLength(50);
+        });
+
+        modelBuilder.Entity<TblProductCategory>(entity =>
+        {
+            entity.HasKey(e => e.ProductCategoryId);
+
+            entity.ToTable("Tbl_ProductCategory");
+
+            entity.Property(e => e.ProductCategoryCode)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.ProductCategoryName)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<TblSaleInvoice>(entity =>
+        {
+            entity.HasKey(e => e.SaleInvoiceId);
+
+            entity.ToTable("Tbl_SaleInvoice");
+
+            entity.Property(e => e.Change).HasColumnType("decimal(18, 2)");
+            entity.Property(e => e.CustomerAccountNo).HasMaxLength(20);
+            entity.Property(e => e.CustomerCode).HasMaxLength(50);
+            entity.Property(e => e.Discount).HasColumnType("decimal(18, 2)");
+            entity.Property(e => e.PaymentAmount).HasColumnType("decimal(18, 2)");
+            entity.Property(e => e.PaymentType).HasMaxLength(10);
+            entity.Property(e => e.ReceiveAmount).HasColumnType("decimal(18, 2)");
+            entity.Property(e => e.SaleInvoiceDateTime).HasColumnType("datetime");
+            entity.Property(e => e.StaffCode).HasMaxLength(50);
+            entity.Property(e => e.Tax).HasColumnType("decimal(18, 2)");
+            entity.Property(e => e.TotalAmount).HasColumnType("decimal(18, 2)");
+            entity.Property(e => e.VoucherNo).HasMaxLength(20);
+        });
+
+        modelBuilder.Entity<TblSaleInvoiceDetail>(entity =>
+        {
+            entity.HasKey(e => e.SaleInvoiceDetailId);
+
+            entity.ToTable("Tbl_SaleInvoiceDetail");
+
+            entity.Property(e => e.Amount).HasColumnType("decimal(18, 2)");
+            entity.Property(e => e.Price).HasColumnType("decimal(18, 2)");
+            entity.Property(e => e.ProductCode).HasMaxLength(50);
+            entity.Property(e => e.VoucherNo).HasMaxLength(20);
+        });
+
+        modelBuilder.Entity<TblShop>(entity =>
+        {
+            entity.HasKey(e => e.ShopId);
+
+            entity.ToTable("Tbl_Shop");
+
+            entity.Property(e => e.Address)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.MobileNo)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.ShopCode)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.ShopName)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<TblStaff>(entity =>
+        {
+            entity.HasKey(e => e.StaffId);
+
+            entity.ToTable("Tbl_Staff");
+
+            entity.Property(e => e.Address)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.DateOfBirth).HasColumnType("datetime");
+            entity.Property(e => e.Gender)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.MobileNo)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Password)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Position)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.StaffCode)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.StaffName)
+                .HasMaxLength(50)
+                .IsUnicode(false);
         });
 
         modelBuilder.Entity<TblTransactionHistory>(entity =>
---
+++ MTTKDotNetCore.Database/Models/TblProductCategory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MTTKDotNetCore.Database.Models;
+
+public partial class TblProductCategory
+{
+    public int ProductCategoryId { get; set; }
+
+    public string ProductCategoryCode { get; set; } = null!;
+
+    public string ProductCategoryName { get; set; } = null!;
+}
