f4239c1d56755a090d7ce8e77aede11a3200367e
MiThetThetKhaing <mithetthetkhaing.lashio@gmail.com>
2024-11-20 11:50:24 +0630

    feat: update TransferService and Model

--- MTTKDotNetCore.Domain/Features/Account/TransferService.cs
+++ MTTKDotNetCore.Domain/Features/Account/TransferService.cs
@@ -1,9 +1,12 @@
 ﻿using Azure;
 using Microsoft.EntityFrameworkCore;
 using MTTKDotNetCore.Database.Models;
+using MTTKDotNetCore.Domain.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Reflection;
 using System.Security.Cryptography;
 using System.Text;
@@ -15,56 +18,97 @@ namespace MTTKDotNetCore.Domain.Features.Account
     {
         private readonly AppDbContext _db = new AppDbContext();
 
-        public bool? CreateTransfer(string fromMobile, string toMobile, decimal amount, string pin, string notes)
+        public async Task<Result<ResultTransferResponseModel>> CreateTransfer(string fromMobile, string toMobile, decimal amount, string pin, string notes)
         {
-            var fromPhone = _db.TblAccounts.AsNoTracking().Where(x => x.DeleteFlag == false).FirstOrDefault(x => x.MobileNo == fromMobile);
-            var toPhone = _db.TblAccounts.AsNoTracking().Where(x => x.DeleteFlag == false).FirstOrDefault(x => x.MobileNo == toMobile);
+            //TransferResponseModel model = new TransferResponseModel();
+            Result<ResultTransferResponseModel> model = new Result<ResultTransferResponseModel>();
 
-            if (fromPhone != null && toPhone != null)
+            var fromPhone = await _db.TblAccounts.AsNoTracking().Where(x => x.DeleteFlag == false).FirstOrDefaultAsync(x => x.MobileNo == fromMobile);
+            var toPhone = await _db.TblAccounts.AsNoTracking().Where(x => x.DeleteFlag == false).FirstOrDefaultAsync(x => x.MobileNo == toMobile);
+
+            TblTransactionHistory history = new TblTransactionHistory();
+
+            #region Notes Check
+            if (string.IsNullOrEmpty(notes))
             {
-                if (fromPhone.MobileNo != toPhone.MobileNo)
-                {
-                    if (fromPhone.Pin == pin)
-                    {
-                        if (amount > fromPhone.Balance || 10000 > fromPhone.Balance - amount)
-                        {
-                            return null;
-                        }
-                        else
-                        {
-                             fromPhone.Balance -= amount;
-                             toPhone.Balance += amount;
-
-                            _db.TblAccounts.Update(fromPhone);
-                            _db.TblAccounts.Update(toPhone);
-                            _db.SaveChanges();
-
-                            TblTransactionHistory history = new TblTransactionHistory();
-                            history.FromMobileNo = fromMobile;
-                            history.ToMobileNo = toMobile;
-                            history.Amount = amount;
-                            history.Notes = notes;
-                            history.TranDate = DateTime.Now;
-
-                            _db.TblTransactionHistories.Add(history);
-                            _db.SaveChanges();
-                        }
-                    }
-                    else
-                    {
-                        return null;
-                    }
-                }
-                else
-                {
-                    return null;
-                }
+                //model.Response = BaseResponseModel.ValidationError("999", "Notes cannot be blank.");
+                model = Result<ResultTransferResponseModel>.ValidationError("Notes cannot be blank.");
+                return model;
             }
-            else
+            #endregion
+
+            #region PhoneNo Check
+            if (fromPhone.MobileNo == toPhone.MobileNo)
             {
-                return null;
+                //model.Response = BaseResponseModel.ValidationError("999", "Phone no can't not be the same.");
+                model = Result<ResultTransferResponseModel>.ValidationError("Phone no can't not be the same.");
+                return model;
             }
-            return true;
-        }
+
+            if (fromPhone is null)
+            {
+                //model.Response = BaseResponseModel.ValidationError("999", "Sender phone no doesn't exist.");
+                model = Result<ResultTransferResponseModel>.ValidationError("Sender phone no doesn't exist.");
+                return model;
+                //goto Result; 
+            }
+            #endregion
+
+            #region Pin Check
+            if (fromPhone.Pin != pin)
+            {
+                //model.Response = BaseResponseModel.ValidationError("999", "Pin is not correct.");
+                model = Result<ResultTransferResponseModel>.ValidationError("Incorrect Pin");
+                return model;
+            }
+            #endregion
+
+            #region Receiver Phone Check
+            if (toPhone is null)
+            {
+                //model.Response = BaseResponseModel.ValidationError("999", "Receiver phone no doesn't exist.");
+                model = Result<ResultTransferResponseModel>.ValidationError("Receiver phone no dees");
+                return model;
+            }
+            #endregion
+
+            #region Amount Check
+            if (amount > fromPhone.Balance || 10000 > fromPhone.Balance - amount)
+            {
+                //model.Response = BaseResponseModel.ValidationError("999", "Insufficient Balance.");
+                model = Result<ResultTransferResponseModel>.ValidationError("Insufficient Balance.");
+                return model;
+            }
+            #endregion
+
+
+            fromPhone.Balance -= amount;
+            toPhone.Balance += amount;
+
+            _db.TblAccounts.Update(fromPhone);
+            _db.TblAccounts.Update(toPhone);
+            await _db.SaveChangesAsync();
+
+            history.FromMobileNo = fromMobile;
+            history.ToMobileNo = toMobile;
+            history.Amount = amount;
+            history.Notes = notes;
+            history.TranDate = DateTime.Now;
+
+            _db.TblTransactionHistories.Add(history);
+            await _db.SaveChangesAsync();
+
+            ResultTransferResponseModel result = new ResultTransferResponseModel
+            {
+                TransactionHistory = history
+            };
+
+            //model.Response = BaseResponseModel.Success("000", "Success.");
+            model = Result<ResultTransferResponseModel>.Success(result, "Success");
+
+        //Result:
+            return model;
+        }      
     }
 }
+
---
+++ MTTKDotNetCore.Domain/Models/TransferResponseModel.cs
@@ -0,0 +1,18 @@
+﻿using MTTKDotNetCore.Database.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MTTKDotNetCore.Domain.Models;
+
+public class TransferResponseModel
+{
+    public BaseResponseModel Response { get; set; }
+}
+
+public class ResultTransferResponseModel
+{
+    public TblTransactionHistory TransactionHistory { get; set; }
+}
