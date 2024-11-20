7dc4ec71e6bf83fa2ecce2ecf8ca1db5e3596d0d
MiThetThetKhaing <mithetthetkhaing.lashio@gmail.com>
2024-11-20 11:49:39 +0630

    feat: update MiniKpayController

--- MTTKDotNetCore.AccountApi/Controllers/TransferController.cs
+++ MTTKDotNetCore.AccountApi/Controllers/TransferController.cs
@@ -1,21 +1,36 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MTTKDotNetCore.Database.Models;
 using MTTKDotNetCore.Domain.Features.Account;
+using MTTKDotNetCore.Domain.Models;
+using MTTKDotNetCore.MiniKpayApi.Controllers;
 
 namespace MTTKDotNetCore.AccountApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class TransferController : Controller
+    public class TransferController : BaseController
     {
         private readonly TransferService _transferService = new TransferService();
 
         [HttpPost]
-        public IActionResult Transfer (string fromMobile, string toMobile, decimal amount, string pin, string notes)
+        public async Task<IActionResult> Transfer (string fromMobile, string toMobile, decimal amount, string pin, string notes)
         {
-            var result = _transferService.CreateTransfer(fromMobile, toMobile, amount, pin, notes);
+            var result = await _transferService.CreateTransfer(fromMobile, toMobile, amount, pin, notes);
+            var result2 = await _transferService.CreateTransfer(fromMobile, toMobile, amount, pin, notes);
 
-            return Ok(result);
+            //if (result.Response.IsSuccess) 
+            //    return Ok(result);
+
+            //if (result.Response.RespType == EnumRespType.ValidationError)
+            //    return BadRequest(result);
+
+            //if (result.Response.RespType == EnumRespType.SystemError)
+            //    return StatusCode(500, result);
+
+            //return Ok(result);
+
+            //return Execute(result);
+            return Execute(result2);
         }
     }
 }
