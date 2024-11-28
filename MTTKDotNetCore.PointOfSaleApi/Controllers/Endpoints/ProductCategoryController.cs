139c69eaa4828f491fde770c51f67928ef164ce6
MiThetThetKhaing <mithetthetkhaing.lashio@gmail.com>
2024-11-28 22:53:41 +0630

    feat: add controller

---
+++ MTTKDotNetCore.PointOfSaleApi/Controllers/Endpoints/ProductCategoryController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MTTKDotNetCore.PointOfSale.Api;
+using MTTKDotNetCore.PointOfSale.Database.Models;
+using MTTKDotNetCore.PointOfSale.Domain.Features;
+using MTTKDotNetCore.PointOfSale.Domain.Models;
+
+namespace MTTKDotNetCore.PointOfSaleApi.Controllers.Endpoints
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductCategoryController : BaseController
+    {
+        private readonly ProductCategoryService _service;
+
+        public ProductCategoryController()
+        {
+            _service = new ProductCategoryService();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetList()
+        {
+            var result = await _service.GetProductCategories();
+            return Execute(result);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var result = await _service.GetProductCategory(id);
+            return Execute(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(TblProductCategory productCategory)
+        {
+            var result = await _service.CreateProductCategories(productCategory);
+            return Execute(result);
+        }
+
+        [HttpPatch("{id}")]
+        public async Task<IActionResult> Update(int id, TblProductCategory productCategory)
+        {
+            var result = await _service.UpdateProductCategory(id, productCategory);
+            return Execute(result);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var result = await _service.DeleteProductCategory(id);
+            return Execute(result);
+        }
+    }
+}
---
+++ MTTKDotNetCore.PointOfSaleApi/Controllers/Endpoints/ProductController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MTTKDotNetCore.PointOfSale.Database.Models;
+using MTTKDotNetCore.PointOfSale.Domain.Features;
+
+namespace MTTKDotNetCore.PointOfSale.Api.Controllers.Endpoints
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : BaseController
+    {
+        private readonly ProductService _productService;
+
+        public ProductController()
+        {
+            _productService = new ProductService();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetProducts()
+        {
+            var result = await _productService.GetProducts();
+            return Execute(result);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProduct(int id)
+        {
+            var result = await _productService.GetProduct(id);
+            return Execute(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateProduct(TblProduct product)
+        {
+            var result = await _productService.CreateProduct(product);
+            return Execute(result);
+        }
+
+        [HttpPatch("{id}")]
+        public async Task<IActionResult> UpdateProduct(int id, TblProduct product)
+        {
+            var result = await _productService.UpdateProduct(id, product);
+            return Execute(result);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProduct(int id)
+        {
+            var result = await _productService.DeleteProduct(id);
+            return Execute(result);
+        }
+    }
+}
