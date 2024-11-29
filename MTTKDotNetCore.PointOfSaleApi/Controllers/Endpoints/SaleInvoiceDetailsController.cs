7619cf0817a3264b4f815ca31d2e1c3b2ff4a438
MiThetThetKhaing <mithetthetkhaing.lashio@gmail.com>
2024-11-29 22:06:53 +0630

    feat: update SaleInvoiceDetailController

---
+++ MTTKDotNetCore.PointOfSaleApi/Controllers/Endpoints/SaleInvoiceDetailsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MTTKDotNetCore.PointOfSale.Database.Models;
+using MTTKDotNetCore.PointOfSale.Domain.Features;
+using MTTKDotNetCore.PointOfSale.Domain.Models;
+
+namespace MTTKDotNetCore.PointOfSale.Api.Controllers.Endpoints
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SaleInvoiceDetailsController : BaseController
+    {
+        private readonly SaleInvoiceDetailsService _saleInvoiceDetailsService;
+        public SaleInvoiceDetailsController()
+        {
+            _saleInvoiceDetailsService = new SaleInvoiceDetailsService();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateSaleInvoiceDetail(TblSaleInvoiceDetail saleInvoiceDetail)
+        {
+            var result = await _saleInvoiceDetailsService.CreateSaleInvoiceDetail(saleInvoiceDetail);
+            return Execute(result);
+        }
+    }
+}
