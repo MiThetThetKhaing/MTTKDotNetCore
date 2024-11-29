168eb68d71b1fa7ec4bdb78dfca75494cc20ef95
MiThetThetKhaing <mithetthetkhaing.lashio@gmail.com>
2024-11-30 00:11:31 +0630

    feat: update HttpClient

---
+++ MTTKDotNetCore.ConsoleApp3/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MTTKDotNetCore.ConsoleApp3;
+
+Console.WriteLine("Hello, World!");
+
+// get, post, put, patch, delete
+
+// resource , endpoint
+
+HttpClientExample httpClientExample = new HttpClientExample();
+//await httpClientExample.Read();
+//await httpClientExample.Edit(1);
+//await httpClientExample.Edit(101);
+//await httpClientExample.Create(1, "something", "something");
+
+await httpClientExample.Update(1 ,1, "something", "something");
+
+
