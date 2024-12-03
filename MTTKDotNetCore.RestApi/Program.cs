c50a7a7d04ab213f4e258e91d41d15268d892be6
MiThetThetKhaing <mithetthetkhaing.lashio@gmail.com>
2024-12-03 14:26:14 +0630

    feat: update restApi

--- MTTKDotNetCore.RestApi/Program.cs
+++ MTTKDotNetCore.RestApi/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.EntityFrameworkCore;
+using MTTKDotNetCore.Database.Models;
+using MTTKDotNetCore.Domain.Features.Blog;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,16 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Ui 
+// Business layer
+// Data access
+
+builder.Services.AddDbContext<AppDbContext>(opt =>
+{
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection"));
+});
+builder.Services.AddScoped<BlogService>();
 
 var app = builder.Build();
 
