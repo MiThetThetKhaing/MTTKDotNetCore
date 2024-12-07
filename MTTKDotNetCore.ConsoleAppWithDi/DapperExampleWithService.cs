15704c363724b90b643b2ab3b7e7d484b875eb23
MiThetThetKhaing <mithetthetkhaing.lashio@gmail.com>
2024-12-07 19:22:55 +0630

    feat : new console app with di

---
+++ MTTKDotNetCore.ConsoleAppWithDi/DapperExampleWithService.cs
@@ -0,0 +1,109 @@
+﻿using Dapper;
+using MTTKDotNetCore.ConsoleApp.Models;
+using MTTKDotNetCore.Shared;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MTTKDotNetCore.ConsoleApp
+{
+    public class DapperExampleWithService
+    {
+        private readonly string _connectionString = "Data Source=DESKTOP-6QTP69L\\MSSQLSERVER2022; Initial Catalog=DotNetTrainingBatch5; User ID=sa; Password=sa123;";
+        private readonly DapperService _dapperService;
+
+        public DapperExampleWithService()
+        {
+            _dapperService = new DapperService(_connectionString);
+        }
+
+        public void Read()
+        {
+            string query = "select * from tbl_Blog where DeleteFlag = 0;";
+            var lst = _dapperService.Query<BlogDapperDataModel>(query).ToList();
+            foreach (var item in lst)
+            {
+                Console.WriteLine(item.BlogId);
+                Console.WriteLine(item.BlogTitle);
+                Console.WriteLine(item.BlogAuthor);
+                Console.WriteLine(item.BlogContent);
+            }
+        }
+
+        public void Edit(int id)
+        {
+            string query = "select * from tbl_Blog where DeleteFlag = 0 and BlogId = @BlogId;";
+            var item = _dapperService.QueryFirstOrDefault<BlogDapperDataModel>(query, new BlogDapperDataModel
+            {
+                BlogId = id
+            });
+
+            if (item is null)
+            {
+                Console.WriteLine("No Data Found..");
+                return;
+            }
+
+            Console.WriteLine(item.BlogId);
+            Console.WriteLine(item.BlogTitle);
+            Console.WriteLine(item.BlogAuthor);
+            Console.WriteLine(item.BlogContent);
+        }
+
+        public void Create(string title, string author, string content)
+        {
+            string queryInsert = $@"INSERT INTO [dbo].[Tbl_Blog]
+                                   ([BlogTitle]
+                                   ,[BlogAuthor]
+                                   ,[BlogContent]
+                                   ,[DeleteFlag])
+                             VALUES
+                                   (@BlogTitle
+                                   ,@BlogAuthor
+                                   ,@BlogContent
+                                   ,0)";
+
+            int result = _dapperService.Execute(queryInsert, new
+            {
+                BlogTitle = title,
+                BlogAuthor = author,
+                BlogContent = content,
+            });
+            Console.WriteLine(result == 1 ? "Saving Successful.." : "Saving Failed..");
+        }
+
+        public void Update(int id, string title, string author, string content)
+        {
+            string queryUpdate = @"UPDATE [dbo].[Tbl_Blog]
+                           SET [BlogTitle] = @BlogTitle
+                              ,[BlogAuthor] = @BlogAuthor
+                              ,[BlogContent] = @BlogContent
+                              ,[DeleteFlag] = 0
+                         WHERE BlogId = @BlogId";
+
+            int result = _dapperService.Execute(queryUpdate, new
+            {
+                BlogId = id,
+                BlogTitle = title,
+                BlogAuthor = author,
+                BlogContent = content
+            });
+
+            Console.WriteLine(result == 1 ? "Updating Successfully.." : "Updating Failed..");
+        }
+
+        public void Delete(int id)
+        {
+            string deleteQuery = @"UPDATE [dbo].[Tbl_Blog] SET [DeleteFlag] = 1 WHERE BlogId = @BlogId;";
+            int result = _dapperService.Execute(deleteQuery, new
+            {
+                BlogId = id
+            });
+
+            Console.WriteLine(result == 1 ? "Delete Successfully.." : "Delete Failed..");
+        }
+    }
+}
---
+++ MTTKDotNetCore.ConsoleAppWithDi/Program.cs
@@ -0,0 +1,83 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using Microsoft.Extensions.DependencyInjection;
+using MTTKDotNetCore.ConsoleApp;
+
+//Console.WriteLine("Hello, World!");
+//Console.ReadLine();
+//Console.ReadKey();
+
+// md => markdown 
+
+// C# <=> Database
+
+// ADO.NET
+// Dapper
+// EFCore / EntityFramework (ORM)
+
+// C# => sql query
+
+// nuget
+
+// max connection = 100
+// 100 - 99
+// 101
+
+
+//AdoDotNetExample adoDotNetExample = new AdoDotNetExample();
+//adoDotNetExample.Read();
+//adoDotNetExample.Create();
+//adoDotNetExample.Edit();
+//adoDotNetExample.Update();
+//adoDotNetExample.Delete();
+
+//DapperExample dapperExample = new DapperExample();
+//dapperExample.Read();
+//dapperExample.Create("So Cute", "KKO", "Something");
+//dapperExample.Edit(1);
+//dapperExample.Edit(2);
+//dapperExample.Update(2, "haha", "usus", "update");
+//dapperExample.Delete(3);
+
+//EFCoreExample eFCoreExample = new EFCoreExample();
+//eFCoreExample.Read();
+//eFCoreExample.Create("new", "kkoo", "create new blog");
+//eFCoreExample.Edit(10);
+//eFCoreExample.Update(10, "update", "Mttk", "updating");
+//eFCoreExample.Delete(8);
+
+//AdoDotNetExampleWithService adoDotNetExampleWithService = new AdoDotNetExampleWithService();
+//adoDotNetExampleWithService.Read();
+
+//DapperExampleWithService dapperExampleWithService = new DapperExampleWithService();
+//dapperExampleWithService.Read();
+
+var services = new ServiceCollection().AddSingleton<AdoDotNetExample>().BuildServiceProvider();
+var services2 = new ServiceCollection().AddSingleton<AdoDotNetExampleWithService>().BuildServiceProvider(); 
+var servicesDapper = new ServiceCollection().AddSingleton<DapperExample>().BuildServiceProvider();
+var servicesDapper2 = new ServiceCollection().AddSingleton<DapperExampleWithService>().BuildServiceProvider();
+var servicesEfcore = new ServiceCollection().AddSingleton<EFCoreExample>().BuildServiceProvider();
+
+//var adoDotNetExample = services.GetRequiredService<AdoDotNetExample>();
+//adoDotNetExample.Read();
+
+//var adoDotNetExampleWithService = services2.GetRequiredService<AdoDotNetExampleWithService>();
+//adoDotNetExampleWithService.Read();
+//adoDotNetExampleWithService.Update();
+//adoDotNetExampleWithService.Create();
+//adoDotNetExampleWithService.Delete();
+
+//var dapperExample = servicesDapper.GetRequiredService<DapperExample>();
+//dapperExample.Read();
+//dapperExample.Update(1037, "update dapper", "update dapper author", "update dapper content");
+
+//var dapperExampleWithService = servicesDapper2.GetRequiredService<DapperExampleWithService>();
+//dapperExampleWithService.Read();
+//dapperExampleWithService.Update(6, "update title", "update author", "update content");
+
+var eFCoreExample = servicesEfcore.GetRequiredService<EFCoreExample>();
+eFCoreExample.Read();
+
+Console.ReadKey();
