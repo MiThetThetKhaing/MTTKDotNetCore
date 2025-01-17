f89da812c9bb4820e615ce7f53e026885ba8cb08
MiThetThetKhaing <mithetthetkhaing.lashio@gmail.com>
2025-01-18 00:48:58 +0630

    Add : add blog controller

---
+++ MTTKDotNetCore.MvcApp/Controllers/BlogController.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MTTKDotNetCore.Database.Models;
+using MTTKDotNetCore.Domain.Features.Blog;
+using MTTKDotNetCore.MvcApp.Models;
+
+namespace MTTKDotNetCore.MvcApp.Controllers
+{
+    public class BlogController : Controller
+    {
+        private readonly IBlogService _blogService;
+
+        public BlogController(IBlogService blogService)
+        {
+            _blogService = blogService;
+        }
+
+        public IActionResult Index()
+        {
+            var list = _blogService.GetBlogs();
+            return View(list);
+        }
+
+        [ActionName("Create")]
+        public IActionResult BlogCreate()
+        {
+            return View("BlogCreate");
+        }
+
+        [HttpPost]
+        [ActionName("Save")]
+        public IActionResult BlogSave(BlogRequestModel requestModel)
+        {
+            try
+            {
+                _blogService.CreateBlog(new TblBlog
+                {
+                    BlogTitle = requestModel.Title,
+                    BlogAuthor = requestModel.Author,
+                    BlogContent = requestModel.Content
+                });
+
+                //ViewBag.IsSuccess = true;
+                //ViewBag.Message = "Blog Created Successfully.";
+
+                TempData["IsSuccess"] = true;
+                TempData["Message"] = "Blog Created Successfully";
+            }
+            catch (Exception ex)
+            {
+                //ViewBag.IsSuccess = false;
+                //ViewBag.Message = ex.ToString();
+
+                TempData["IsSuccess"] = false;
+                TempData["Message"] = ex.ToString();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [ActionName("Delete")]
+        public IActionResult BlogDelete(int id)
+        {
+            _blogService.DeleteBlog(id);
+            return RedirectToAction("Index");
+        }
+
+        [ActionName("Edit")]
+        public IActionResult BlogEdit(int id)
+        {
+            var blog = _blogService.GetBlog(id);
+            BlogRequestModel model = new BlogRequestModel()
+            {
+                Id = blog.BlogId,
+                Title = blog.BlogTitle,
+                Author = blog.BlogAuthor,
+                Content = blog.BlogContent
+            };
+            return View("BlogEdit", model);
+        }
+
+        [HttpPost]
+        [ActionName("Update")]
+        public IActionResult BlogUpdate(int id, BlogRequestModel requestModel)
+        {
+            try
+            {
+                _blogService.UpdateBlog(id, new TblBlog
+                {
+                    BlogTitle = requestModel.Title,
+                    BlogAuthor = requestModel.Author,
+                    BlogContent = requestModel.Content
+                });
+
+                TempData["IsSuccess"] = true;
+                TempData["Message"] = "Blog Updated Successfully";
+            }
+            catch (Exception ex)
+            {
+                TempData["IsSuccess"] = false;
+                TempData["Message"] = ex.ToString();
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ MTTKDotNetCore.MvcApp/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MTTKDotNetCore.MvcApp.Models;
+using System.Diagnostics;
+
+namespace MTTKDotNetCore.MvcApp.Controllers
+{
+    // https://localhost:3000/Home/Index
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.Message = "Hello from Viewbag";
+
+            HomeResponseModel model = new HomeResponseModel();
+            model.message = "Hello from model";
+
+            return View(model);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult Index2()
+        {
+            return View();
+        }
+    }
+}
