2f1395f00ec6e7c1dc7f0669c402b37c2e1160a3
MiThetThetKhaing <mithetthetkhaing.lashio@gmail.com>
2025-01-24 21:04:16 +0630

    feat: update high chart model

---
+++ MTTKDotNetCore.ChartWebApp/Models/HighChart3DColumnModel.cs
@@ -0,0 +1,15 @@
+﻿namespace MTTKDotNetCore.ChartWebApp.Models
+{
+    public class HighChart3DColumnModel
+    {
+        public List<SeriesDetail?> Data { get; set; }
+        public bool ColorByPrint { get; set; }
+    }
+
+    public class SeriesDetail
+    {
+        public string Brand { get; set; }
+
+        public int Sales { get; set; }
+    }
+}
---
+++ MTTKDotNetCore.ChartWebApp/Models/HighChartLineChartModel.cs
@@ -0,0 +1,18 @@
+﻿namespace MTTKDotNetCore.ChartWebApp.Models
+{
+    public class HighChartLineChartModel
+    {
+        public string Title { get; set; }
+        public string SubTitle { get; set; }
+        public string YAxis { get; set; }
+        public string XAxis { get; set; }
+        public List<SerieData?> Series { get; set; }
+    }
+
+    public class SerieData
+    {
+        public string Name { get; set; }
+
+        public List<int?> Data { get; set; }
+    }
+}
